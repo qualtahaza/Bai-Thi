afdd264c6d4ffa2e48e6ea9298b9bcdca583121c
TienAnh <qualtahaza@gmail.com>
2021-03-30 15:19:48 +0700

    add B

--- B/app.js
+++ B/app.js
@@ -1,12 +1,22 @@
 let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+
 console.log(randomNumber);
+
 function checkNumber() {
     let x = document.getElementById('textbox').value;
 
-    if (x == randomNumber) {
-        alert("bạn đã trúng thưởng ");
-    } else {
-        alert("bạn đã ko trúng thưởng, số trúng thưởng sẽ là " + randomNumber);
-    }
 
+    if (Number.isInteger(parseInt(x)) && x > 0 && x <= 10) {
+        if (x == randomNumber) {
+            alert("bạn đã chọn đúng số trúng thưởng ");
+
+        } else {
+            alert("bạn đã ko trúng thưởng, số trúng thưởng lượt này là " + randomNumber);
+
+        }
+    } else {
+        alert("Giá trị bạn nhập không đúng định dạng")
+    }
+    location.reload();
 }
